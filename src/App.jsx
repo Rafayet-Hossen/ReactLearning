b9660b2d1504c7d25349d1323acd9745c94f91cc
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-10-16 17:24:44 +0600

    Learning Props with distructering

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Devices name="laptop" price= "70K" ></Devices>
-      <Devices name="Watch" price= "1K" ></Devices>
-      <Devices name="Phone" price= "30K" ></Devices>
-      <Devices name="Tablet" price= "45K" ></Devices>
+      <React></React>
+      {/* <Devices name="laptop" price="70K"></Devices>
+      <Devices name="Watch" price="1K"></Devices>
+      <Devices name="Phone" price="30K"></Devices>
+      <Devices name="Tablet" price="45K"></Devices>
+
+      <Student id="1" name="Rafayet" grade="A"></Student>
+      <Student id="2" name="Saikot" grade="A"></Student>
+      <Student id="3" name="Alif" grade="A"></Student>
+      <Student id="4" name="Liton" grade="A"></Student> */}
     </>
-  )
+  );
 }
 
-function Student() {
+function React() {
   return (
-    <div className='student'>
-      <h1>Student Component</h1>
-      <p>I am learning react</p>
+    <div>
+      <h1>I am learning react</h1>
     </div>
-  )
+  );
 }
 
 function Devices(props) {
-
   return (
-    <div className='devices'>
-       <p>This is a {props.name} and price is {props.price} </p>
+    <div>
+      <div className="devices">
+        <p>
+          This is a {props.name} and price is {props.price}{" "}
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+// Advanced Way
+
+function Student({ id, name, grade }) {
+  return (
+    <div className="card">
+      <div className="student">
+        <h4>Student Id: {id}</h4>
+        <p>Name: {name}</p>
+        <p>Grade: {grade}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
